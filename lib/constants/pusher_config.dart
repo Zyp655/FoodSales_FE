9912a4c4d48cce69a77499d08d695c74a4f4fb0e
Zyp655 <leehieu655@gmail.com>
2025-11-08 20:37:51 +0700

    refactor: update Pusher auth endpoint URL
    Updates the authentication endpoint for the broadcasting service to use the `/api` prefix.

--- lib/constants/pusher_config.dart
+++ lib/constants/pusher_config.dart
@@ -4,5 +4,5 @@ class PusherConfig {
   static const String host = "10.0.2.2";
   static const int port = 6001;
 
-  static const String authEndpoint = "http://10.0.2.2:8000/broadcasting/auth";
+  static const String authEndpoint = "http://10.0.2.2/api/broadcasting/auth";
 }
